c8f35bbcd167a8553c4c52725df79647e385eb71
iamnovichek <rungotika@gmail.com>
2024-12-31 19:18:46 +0100

    docstring added

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,16 @@
+"""
+core/__init__.py
+================
+
+This module serves as the entry point for the `core` package of the project. The `core` package is typically
+designed to contain fundamental components, utilities, or functionality that is used throughout the project.
+
+Module Overview:
+----------------
+The `core` package acts as a central hub for core utilities, shared components, or foundational services required
+by other parts of the application. Specific functionality or implementations for the project may be further
+broken into submodules or packages within `core`.
+"""
 from .celery import app as celery_app
 
 __all__ = ["celery_app"]
